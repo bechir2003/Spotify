741d4fd9d21fcfa34e3e928ee6635e7ab6ab838b
bechir2003 <bechir.mejri@esprit.tn>
2025-08-20 10:56:53 +0100

    Initial commit - Flask app with HTML

--- spotify1.py
+++ spotify1.py
@@ -163,7 +163,7 @@ def youtube_audio():
         'format': 'bestaudio/best',
         'quiet': True,
         'noplaylist': True,
-        'cookies_from_browser': ('chrome',)
+        'cookiefile': 'cookies.txt'
     }
 
     try:
